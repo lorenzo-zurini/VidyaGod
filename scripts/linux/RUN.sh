ae0259e22cd48009ea6231354db159fb1cee0406
Lorenzo Zurini <lorenzo.zurini@icloud.com>
2024-10-12 19:17:23 +0300

    Removed leftover files.

--- scripts/linux/RUN.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-WINEPREFIX="/home/lorenzo-zurini/RUNTIME/" \
-GAMEID="aoe2" \
-PROTONPATH="/usr/share/steam/compatibilitytools.d/proton-ge-custom/" \
-gamescope -f -b --framerate-limit 200 --force-grab-cursor --adaptive-sync \
-umu-run "C:/Program Files (x86)/Microsoft Games/Age of Empires II/age2_x1/age2_x1.exe" game=age2_x2
-#WINEDLLOVERRIDES="winmm=n,b"
--- scripts/linux/UNBUILD_13DIRS.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-umount "/home/lorenzo-zurini/.wine/"
